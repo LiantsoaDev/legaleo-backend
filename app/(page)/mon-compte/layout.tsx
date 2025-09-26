c4ccf608c5f17f0065858c8b20560d7e560b2237
fastfabien <fastfabien56@gmail.com>
2025-09-26 10:51:17 +0300

    New project: integration text editor

--- app/(page)/mon-compte/layout.tsx
+++ app/(page)/mon-compte/layout.tsx
@@ -1,22 +1,55 @@
 "use client";
 
-import { HeaderDashboard } from "@/components/Header";
+import { CreditCard, HeaderDashboard } from "@/components/Header";
 import { Left } from "@/components/Pages/MonCompte";
+import { Recharge } from "@/utils/types";
+import { useState } from "react";
+
+const recharges: Recharge[] = [
+  {
+    duration: "5h",
+    prix: "100€",
+  },
+  {
+    duration: "10h",
+    prix: "100€",
+  },
+  {
+    duration: "20h",
+    prix: "100€",
+  },
+  {
+    duration: "30h",
+    prix: "100€",
+  },
+];
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [showCreditCard, setShowCreditCard] = useState(false);
   return (
     <div className="flex h-screen flex-col gap-0 bg-dark-green pb-5 pr-8">
-      <HeaderDashboard />
+      <HeaderDashboard
+        setShowCreditCard={setShowCreditCard}
+        showCredit={showCreditCard}
+      />
       <div className="flex h-[85%] flex-row gap-0 px-3">
         <Left />
         <div className="w-4/5 bg-white rounded-2xl py-8 px-10 shadow-md relative overflow-y-auto scrollable">
           {children}
         </div>
       </div>
+      {showCreditCard && (
+        <CreditCard
+          setShow={setShowCreditCard}
+          consommer="5h"
+          restant="12h"
+          rechargers={recharges}
+        />
+      )}
     </div>
   );
 }
--- components/User/User.tsx
+++ components/User/User.tsx
@@ -14,7 +14,7 @@ export const User = () => {
       {showMenu && (
         <div className="py-3.5 px-3.5 flex flex-col gap-0 bg-white shadow-lg rounded-2xl absolute top-15 right-[-20px] w-[10rem] z-5">
           <Link href="/mon-compte" className="font-semibold text-sm mb-5">
-            Réglage
+            Mon Compte
           </Link>
           <span
             className="cursor-pointer font-semibold text-sm"
