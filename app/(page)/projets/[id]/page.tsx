b19a709a87ed0183fe75eb8c7effff6f15995a0a
fastfabien <fastfabien56@gmail.com>
2025-10-17 13:44:14 +0300

    New project: editor via shadcnui

--- app/(page)/projets/[id]/page.tsx
+++ app/(page)/projets/[id]/page.tsx
@@ -1,26 +1,8 @@
 "use client";
 import { Editor } from "@/components/blocks/editor-00/editor";
 
-const page = ({
-  currentTab,
-  setShowTab,
-  showTab,
-  handleShowTab,
-}: {
-  currentTab?: number;
-  showTab?: boolean;
-  setShowTab?: React.Dispatch<React.SetStateAction<boolean>>;
-  handleShowTab?: any;
-}) => {
-  console.log(showTab);
-  return (
-    <Editor
-      currentTab={currentTab}
-      setShowTab={setShowTab}
-      showTab={showTab}
-      handleShowTab={handleShowTab}
-    />
-  );
+const page = ({}: {}) => {
+  return <Editor />;
 };
 
 export default page;
