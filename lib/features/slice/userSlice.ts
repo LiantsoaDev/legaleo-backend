26d287b05402de63bcdd04b0dce0c769ab981fda
liantsoa.rakotoarinia <tsorakoto@gmail.com>
2025-11-14 21:46:20 +0300

    Prefill onboarding user information inputs

--- lib/features/slice/userSlice.ts
+++ lib/features/slice/userSlice.ts
@@ -3,6 +3,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 interface UserState {
   id: string | null;
   name: string | null;
+  lastName: string | null;
   email: string | null;
   image?: string | null;
   isAuthenticated: boolean;
@@ -11,6 +12,7 @@ interface UserState {
 const initialState: UserState = {
   id: null,
   name: null,
+  lastName: null,
   email: null,
   image: null,
   isAuthenticated: false,
@@ -25,12 +27,14 @@ const userSlice = createSlice({
       action: PayloadAction<{
         id: string;
         name: string;
+        lastName?: string | null;
         email: string;
         image?: string;
       }>
     ) => {
       state.id = action.payload.id;
       state.name = action.payload.name;
+      state.lastName = action.payload.lastName ?? null;
       state.email = action.payload.email;
       state.image = action.payload.image;
       state.isAuthenticated = true;
@@ -38,6 +42,7 @@ const userSlice = createSlice({
     clearUser: (state) => {
       state.id = null;
       state.name = null;
+      state.lastName = null;
       state.email = null;
       state.image = null;
       state.isAuthenticated = false;
